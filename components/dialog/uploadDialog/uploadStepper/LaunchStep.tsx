a18db4ca630b39f5d831a35d335f0eeb0986a357
iEstelle211 <iEstelle211@gmail.com>
2023-11-11 00:36:52 +0900

    complete skeleton steps

--- components/dialog/uploadDialog/uploadStepper/LaunchStep.tsx
+++ components/dialog/uploadDialog/uploadStepper/LaunchStep.tsx
@@ -1,20 +1,23 @@
 import { StepWrapper } from './index'
 import { Box } from '@mui/material'
+import LinearProgress from '@mui/material/LinearProgress'
+import React from 'react'
 
 
 export default function LaunchStep() {
 
-  const [progress, setProgress] = React.useState(0)
+  const [vState, setState] = React.useState({ progress:0 })
 
   React.useEffect(() => {
     const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
+      const setProgress=(state) => {
+        if (state.progress === 100) {
           return 0
         }
         const diff = Math.random() * 10
-        return Math.min(oldProgress + diff, 100)
-      })
+        return { ...state,progress:Math.min(state.progress + diff, 100) }
+      };
+      setState(setProgress)
     }, 500)
 
     return () => {
@@ -31,6 +34,27 @@ export default function LaunchStep() {
       >
         <div>
           Video Upload
+        </div>
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress variant="determinate" value={vState.progress} />
+        </Box>
+        <div className='flex justify-between'>
+          <div>
+            Uploading
+          </div>
+          <div>
+            Analyzing
+          </div>
+          <div>
+            Indexing
+          </div>
+          <div>
+            Scoring
+          </div>
+        </div>
+
+        <div>
+          Disclaimer: He should not leave this page until the first stage of upload is done.
         </div>
 
       </Box>
