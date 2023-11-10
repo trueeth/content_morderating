28d8bdcbe86f6e5112993e8912be7725f4461574
iEstelle211 <iEstelle211@gmail.com>
2023-11-10 20:41:00 +0900

    create Drawertabs

--- pages/video/drawer/DrawerTabOverview.tsx
+++ pages/video/drawer/DrawerTabOverview.tsx
@@ -1,58 +1,113 @@
 import { Box } from '@mui/material'
 import { styled } from '@mui/styles'
+import Image from 'next/image'
+
+import overview1 from '/assets/images/overview/overview-01.png'
+import overview2 from '/assets/images/overview/overview-02.png'
+import overview3 from '/assets/images/overview/overview-03.png'
+import overview4 from '/assets/images/overview/overview-04.png'
+import overview5 from '/assets/images/overview/overview-05.png'
+import overview6 from '/assets/images/overview/overview-06.png'
+import overview7 from '/assets/images/overview/overview-07.png'
+import overview8 from '/assets/images/overview/overview-08.png'
+import overview9 from '/assets/images/overview/overview-09.png'
+import overview10 from '/assets/images/overview/overview-10.png'
+import overview11 from '/assets/images/overview/overview-11.png'
+import overview12 from '/assets/images/overview/overview-12.png'
+
+
 
 const itemData = [
   {
-    img: 'images/overview/overview-01.png',
+    img: overview1,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-02.png',
+    img: overview2,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-03.png',
+    img: overview3,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-04.png',
+    img: overview4,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-05.png',
+    img: overview5,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-06.png',
+    img: overview6,
     title: 'overview',
   },
   {
-    img: 'images/overview/overview-07.png',
+    img: overview7,
     title: 'overview',
   },
-]
+  {
+    img: overview8,
+    title: 'overview',
+  },
+  {
+    img: overview9,
+    title: 'overview',
+  },
+  {
+    img: overview10,
+    title: 'overview',
+  },
+  {
+    img: overview11,
+    title: 'overview',
+  },
+  {
+    img: overview12,
+    title: 'overview',
+  },
+];
 
-const ImageItem = styled('img')({
-  padding: '.5rem',
-  borderRadius: '1rem',
-})
 
-export default function DrawerTabOverview() {
-  return (
+const ImageItemStyle={
+  margin:'.3rem',
+  borderRadius:'.3rem',
+  width:'10rem',
+  height:'7.5rem',
+}
+
+export default function DrawerTabOverview(){
+
+  return(
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
+        display:'flex',
+        flexDirection:'column'
       }}
     >
-      <div>Most Interest Frames</div>
-      <div className="flex flex-wrap">
-        {itemData.map((item, index) => {
-          return (
-            <ImageItem key={index} src={item.img} alt={item.title}></ImageItem>
-          )
-        })}
+        <div>
+          Most Interest Frames
+        </div>
+        <div className='flex flex-wrap'>
+          {itemData.map((item,index)=>{
+            return(
+              <Image
+                key={index}
+                src={item.img}
+                alt={item.title}
+                style={ImageItemStyle}
+                width={80}
+                height={50}
+              />
+            )
+          })}
+        </div>
+      <div>AI Description</div>
+
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem  Ipsum is simply dummy text of the best ipsum has been the top offer industry's standard printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem  Ipsum is simply dummy text of the best ipsum has been the top industry's standard printing and type setting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem  Ipsum is simply dummy text of the best ipsum has been the top offer industry's standard printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       </div>
     </Box>
   )
+
 }
--- pages/video/drawer/DrawerTabPlayScene.tsx
+++ pages/video/drawer/DrawerTabPlayScene.tsx
@@ -1,5 +1,48 @@
 import { Box } from '@mui/material'
+import { TimeIndicator } from 'react-all-player'
 
-export default function DrawerTabPlayScene() {
-  return <Box>player scene</Box>
+function ReactAllPlayer(props: { components: { Controls: () => JSX.Element } }) {
+  return null
+}
+
+export default function DrawerTabPlayScene(){
+
+  return(
+    <Box
+      sx={{
+        display:'flex',
+        flexDirection:'column'
+      }}
+    >
+
+      <div className='player-wrapper'>
+        <ReactAllPlayer
+          sources={[
+            {
+              file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+              label: '1080p',
+            },
+            {
+              file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+              label: '720p',
+            },
+          ]}
+          subtitles={[
+            {
+              lang: 'en',
+              language: 'English',
+              file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
+            },
+            {
+              lang: 'fr',
+              language: 'French',
+              file: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt',
+            },
+          ]}
+        />
+      </div>
+
+    </Box>
+  )
+
 }
