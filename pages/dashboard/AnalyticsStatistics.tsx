4caf892223b4906d6c5a72026672b3106b989f11
iEstelle211 <iEstelle211@gmail.com>
2023-11-15 06:44:14 +0900

    complete anylatics components

--- pages/dashboard/AnalyticsStatistics.tsx
+++ pages/dashboard/AnalyticsStatistics.tsx
@@ -6,8 +6,9 @@ import {
   HourglassEmptyOutlined,
   InsertDriveFileOutlined,
   Slideshow,
-  StarBorder
+  StarBorder,
 } from '@mui/icons-material'
+import { Grid } from '@mui/material'
 
 type TStatisticsItem = {
   children: React.ReactElement
@@ -18,37 +19,54 @@ type TStatisticsItem = {
 const StatisticsItem = (props: TStatisticsItem) => {
   const ItemIcon = props.children
   return (
-    <div>
+    <div className='flex'>
       <IconButton>{ItemIcon}</IconButton>
 
-      <div>
-        <div>{props.downloads}</div>
-        <div>{props.title}</div>
+      <div className='flex flex-col ml-3'>
+        <div className='text-bold'>{props.downloads}</div>
+        <div className='text-8'>{props.title}</div>
       </div>
     </div>
   )
 }
-export default function () {
+export default function() {
   return (
-    <div className="bg-white border-radius-5 h-full p-15 text-black">
-      <StatisticsItem downloads={856} title={'Submited Videos'}>
-        <Slideshow></Slideshow>
-      </StatisticsItem>
-      <StatisticsItem downloads={428} title={'Submited Documents'}>
-        <InsertDriveFileOutlined></InsertDriveFileOutlined>
-      </StatisticsItem>
-      <StatisticsItem downloads={85} title={'In Review'}>
-        <StarBorder></StarBorder>
-      </StatisticsItem>
-      <StatisticsItem downloads={265} title={'Approved'}>
-        <Check></Check>
-      </StatisticsItem>
-      <StatisticsItem downloads={23} title={'Rejected'}>
-        <Block></Block>
-      </StatisticsItem>
-      <StatisticsItem downloads={136} title={'Pending'}>
-        <HourglassEmptyOutlined></HourglassEmptyOutlined>
-      </StatisticsItem>
+    <div className='bg-white border-radius-5 h-full p-15 text-black'>
+      <div>
+        Statistics
+      </div>
+      <Grid container className='mt-15' spacing={2}>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={856} title={'Submited Videos'}>
+            <Slideshow sx={{color:'var(--Primary1)'}}></Slideshow>
+          </StatisticsItem>
+        </Grid>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={428} title={'Submited Documents'}>
+            <InsertDriveFileOutlined sx={{color:'var(--Primary2)'}}></InsertDriveFileOutlined>
+          </StatisticsItem>
+        </Grid>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={85} title={'In Review'}>
+            <StarBorder sx={{color:'#FF9E45'}}></StarBorder>
+          </StatisticsItem>
+        </Grid>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={265} title={'Approved'}>
+            <Check sx={{color:'var(--Primary1)'}}></Check>
+          </StatisticsItem>
+        </Grid>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={23} title={'Rejected'}>
+            <Block sx={{color:'#FF1313'}}></Block>
+          </StatisticsItem>
+        </Grid>
+        <Grid item xs={6}>
+          <StatisticsItem downloads={136} title={'Pending'}>
+            <HourglassEmptyOutlined sx={{color:'var(--Primary2)'}}></HourglassEmptyOutlined>
+          </StatisticsItem>
+        </Grid>
+      </Grid>
     </div>
   )
 }
